46bc7743b432d13010632311d20a08839170f90c
zaidalyafeai <alyafey22@gmail.com>
2021-01-10 21:11:43 +0300

    debug

--- datasets.py
+++ datasets.py
@@ -12,7 +12,7 @@ class Dataset:
             out_path = resize_dirs(self.path, 'dataset/resized', dim = self.dim)
             create_from_image_folders(tfrecord_dir, out_path, shuffle, ignore_labels)
         elif with_text:
-            out_path = resize_dirs(self.path, 'dataset/resized', dim = self.dim)
+            out_path = resize(self.path, dim = self.dim)
             create_image_and_text(tfrecord_dir, out_path, self.path, shuffle, ignore_labels)
         else:
             print('resizing images ...')
